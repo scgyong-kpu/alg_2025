3cbbc25fac3312f71df284ab590b12fe69c45e84
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-09-28 18:30:53 +0900

    ---------------- Ch3 - 1. Merge Sort 시작 ----------------

---
+++ week05_ch3/ch3_1_merge_sort.py
@@ -0,0 +1,23 @@
+from pyvisalgo import MergeSortVisualizer as Visualizer
+# from pyvisalgo import Dummy as Visualizer
+from time import time
+from random import randint, seed, shuffle
+
+def main():
+  print('before:', array)
+  count = len(array)
+  print('after :', array)
+
+if __name__ == '__main__':
+  seed('Hello')
+  vis = Visualizer('Merge Sort')
+  while True:
+    count = randint(20, 40)
+    array = [ randint(1, 99) for _ in range(count) ]
+    vis.setup(vis.get_main_module())
+    main()
+    vis.draw()
+    again = vis.end()
+    if not again: break
+
+    
