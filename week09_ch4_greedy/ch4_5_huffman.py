3839ecabf5dae6342044ede78c212875c52bf2f7
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-31 02:02:28 +0900

    ch45hu-1. 글자 수 세기
    (cherry picked from commit 6868c100e244ae084d18f8128faa31c381ef39ae)

--- week09_ch4_greedy/ch4_5_huffman.py
+++ week09_ch4_greedy/ch4_5_huffman.py
@@ -1,4 +1,13 @@
 import json
 with open('data_knapsack.json') as f:
   contents = f.read()
-  print(contents, f'{len(contents)=}')
+  print(f'{len(contents)=}')
+
+counts = dict()
+for ch in contents:
+  if ch in counts:
+    counts[ch] += 1
+  else:
+    counts[ch] = 1
+
+print(counts)
