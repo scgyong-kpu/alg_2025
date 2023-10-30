6e0da02b820b069b2b74b15096c32ec06f5c57ff
Kiyong Kim <scgyong@tukorea.ac.kr>
2023-10-31 02:03:52 +0900

    ch45hu-2. dict() 대신 defaultdict() 사용

--- week09_ch4_greedy/ch4_5_huffman.py
+++ week09_ch4_greedy/ch4_5_huffman.py
@@ -1,13 +1,13 @@
-import json
+from collections import defaultdict
+
 with open('data_knapsack.json') as f:
   contents = f.read()
   print(f'{len(contents)=}')
 
-counts = dict()
+counts = defaultdict(int)
 for ch in contents:
-  if ch in counts:
     counts[ch] += 1
-  else:
-    counts[ch] = 1
+    # ch 가 key 로 존재하지 않으면 int() 가 수행되어 값이 들어간다
+    # dict 의 KeyError 를 피할 수 있는 편리한 방법이다
 
 print(counts)
