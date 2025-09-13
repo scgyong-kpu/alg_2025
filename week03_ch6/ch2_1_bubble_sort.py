3c7ee17b2f36fbc2fe0088bab49e8af1cc4b2bfc
KKY@Home <scgyong@tukorea.ac.kr>
2025-09-13 23:37:13 +0900

    Python 3.13 으로 테스트한 결과 추가 적용.

--- week03_ch6/ch2_1_bubble_sort.py
+++ week03_ch6/ch2_1_bubble_sort.py
@@ -33,7 +33,7 @@ count=2000 elapsed=1.794
 count=3000 elapsed=4.001
 count=4000 elapsed=7.062
 
-* Visualizer 코드를 완전히 제거했을 때
+* Visualizer 코드를 완전히 제거했을 때 (3.10 혹은 3.11 로 실행했을 때의 결과)
 count=100 elapsed=0.001
 count=1000 elapsed=0.094
 count=2000 elapsed=0.375
@@ -47,6 +47,22 @@ count=9000 elapsed=7.760
 count=10000 elapsed=9.621
 count=15000 elapsed=21.400
 count=20000 elapsed=39.334
+
+* 2025년 9월 3.13.7 로 테스트한 결과 (3.12/3.13 에서 루프형 코드에 최적화 적용됨)
+count=100 elapsed=0.001
+count=1000 elapsed=0.072
+count=2000 elapsed=0.278
+count=3000 elapsed=0.645
+count=4000 elapsed=1.237
+count=5000 elapsed=2.065
+count=6000 elapsed=2.738
+count=7000 elapsed=3.698
+count=8000 elapsed=4.795
+count=9000 elapsed=5.649
+count=10000 elapsed=7.094
+count=15000 elapsed=16.056
+count=20000 elapsed=28.872
+
 '''
 
 if __name__ == '__main__':
