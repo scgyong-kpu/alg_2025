scgyong-kpu/alg_2023

ch21bs-5. Bubble 을 한 번 진행해 본다

src/ch2_1_bubble_sort.py
@@ -3,10 +3,23 @@ from data_unsorted import numbers
 # numbers = numbers[:1000]
 
 from random import randint, seed
-from vis import BubbleSortVisualizer as Visualizer
+# from vis import BubbleSortVisualizer as Visualizer
+from vis import Dummy as Visualizer
 
 def main():
-  print(array)
+  print('before:', array)
+  count = len(array)
+  end = count - 1
+  if True: # 들여쓰기를 위해 쓴다
+    for i in range(end):
+      if array[i] > array[i+1]:
+        array[i], array[i+1] = array[i+1], array[i]
+  print('after :', array)
+
+''' Bubble 을 한 칸 진행해 본 결과, 다음과 같이 출력된다.
+before: [71, 30, 18, 51, 77, 37, 3, 93, 90, 48]
+after : [30, 18, 51, 71, 37, 3, 77, 90, 48, 93]
+'''
 
 if __name__ == '__main__':
   seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다

scgyong-kpu/alg_2025

week03_ch6/ch2_1_bubble_sort.py
@@ -3,10 +3,24 @@ from data_unsorted import numbers
 # numbers = numbers[:1000]
 
 from random import randint, seed
-from pyvisalgo import BubbleSortVisualizer as Visualizer
+# from pyvisalgo import BubbleSortVisualizer as Visualizer
+from pyvisalgo import Dummy as Visualizer
+# Visualizer 가 나타나지 않게 하려면 Dummy 를 쓰면 된다.
 
 def main():
-  print(array)
+  print('before:', array)
+  count = len(array)
+  end = count - 1
+  if True: # 들여쓰기를 위해 쓴다
+    for i in range(end):
+      if array[i] > array[i+1]:
+        array[i], array[i+1] = array[i+1], array[i]
+  print('after :', array)
+
+''' Bubble 을 한 칸 진행해 본 결과, 다음과 같이 출력된다.
+before: [71, 30, 18, 51, 77, 37, 3, 93, 90, 48]
+after : [30, 18, 51, 71, 37, 3, 77, 90, 48, 93]
+'''
 
 if __name__ == '__main__':
   seed('Hello') # 'Hello' 를 seed 로 고정하여 randint 가 항상 같은 결과가 나오게 한다