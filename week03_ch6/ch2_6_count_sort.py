scgyong-kpu/alg_2023

---------------- Ch6 - 6. Count Sort 시작 ----------------

src/ch2_6_count_sort.py
@@ -1,0 +1,26 @@
+# from data_unsorted import numbers
+from data_unsorted_a_lot import numbers
+from vis import CountSortVisualizer as Visualizer
+# from vis import Dummy as Visualizer
+from time import time
+from random import randint, seed
+
+def main():
+  print('before:', array)
+  count = len(array)
+
+  print('after :', array)
+
+if __name__ == '__main__':
+  seed('HelloCountSort')
+  vis = Visualizer('Count Sort')
+  while True:
+    kind = randint(6, 30)
+    count = randint(30, 150)
+    print(f'Creating data: {kind=} {count=}')
+    array = list(map(lambda x: x%kind, numbers[1000:1000+count]))
+    vis.setup(vis.get_main_module())
+    main()
+    vis.draw()
+    again = vis.end()
+    if not again: break

scgyong-kpu/alg_2025

week03_ch6/ch2_6_count_sort.py
@@ -1,0 +1,26 @@
+# from data_unsorted import numbers
+from data_unsorted_a_lot import numbers
+from pyvisalgo import CountSortVisualizer as Visualizer
+# from pyvisalgo import Dummy as Visualizer
+from time import time
+from random import randint, seed
+
+def main():
+  print('before:', array)
+  count = len(array)
+
+  print('after :', array)
+
+if __name__ == '__main__':
+  seed('HelloCountSort')
+  vis = Visualizer('Count Sort')
+  while True:
+    kind = randint(6, 30)
+    count = randint(30, 150)
+    print(f'Creating data: {kind=} {count=}')
+    array = list(map(lambda x: x%kind, numbers[1000:1000+count]))
+    vis.setup(vis.get_main_module())
+    main()
+    vis.draw()
+    again = vis.end()
+    if not again: break