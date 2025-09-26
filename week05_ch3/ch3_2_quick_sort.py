b91260c4a0f5f36aa738b47f99eda0a03ad4565f
KKY@Home <scgyong@tukorea.ac.kr>
2025-09-27 00:49:01 +0900

    ch32qs-1. 가장 왼쪽의 값을 pivot 으로 삼아서 그 수보다 큰 수를 찾는다.

--- week05_ch3/ch3_2_quick_sort.py
+++ week05_ch3/ch3_2_quick_sort.py
@@ -9,7 +9,29 @@ def main():
   print('before:', array)
   count = len(array)
 
+  vis.push(0, count-1)
+  partition(0, count-1)
+
   print('after :', array)
+
+def partition(left, right):
+
+  pi = left               # pi = Pivot Index
+  pivot = array[pi]       # pivot = Pivot Value
+
+  p, q = left, right + 1  # 후보선수들 출전준비
+
+  if True:                # 나중에 while loop 가 될 예정이다
+    while True:           # 왼쪽에서 pivot 보다 큰 값을 찾을때까지
+      p += 1
+      vis.set_p(p)
+      if q < p: break
+      if p <= right: vis.compare(pi, p)
+      if p > right or array[p] > pivot: break 
+      # 왼쪽에서 pivot 보다 큰 값을 찾았다
+
+    vis.set_left(p)
+    vis.set_right(q)
 
 if __name__ == '__main__':
   seed('Hello')
