ba36a2eac440214a90927de9e2da3e8d7340a930
KKY@Home <scgyong@tukorea.ac.kr>
2025-09-27 01:32:13 +0900

    ch32qs-8. QuickSort 성능측청

--- week05_ch3/ch3_2_quick_sort.py
+++ week05_ch3/ch3_2_quick_sort.py
@@ -1,26 +1,24 @@
-# from data_unsorted import numbers
-# from data_unsorted_a_lot import numbers
-from pyvisalgo import QuickSortVisualizer as Visualizer
+# from pyvisalgo import QuickSortVisualizer as Visualizer
 # from pyvisalgo import Dummy as Visualizer
 from time import time
 from random import randint, seed, shuffle
 
 def main():
-  print('before:', array)
+  # print('before:', array)
   count = len(array)
   quickSort(0, count-1)
 
-  print('after :', array)
+  # print('after :', array)
 
 def quickSort(left, right): #q=inclusive
-  if left == right: vis.fix(left)  # 정렬 대상이 하나뿐이라면 확정해도 좋다
+  # if left == right: vis.fix(left)  # 정렬 대상이 하나뿐이라면 확정해도 좋다
   if left >= right: return         # 정렬할 것이 없으면 할 일이 없다
-  vis.push(left, right)
+  # vis.push(left, right)
   pivot = partition(left, right)   # pivot 위치를 결정해 온다
-  vis.set_pivot(pivot)
+  # vis.set_pivot(pivot)
   quickSort(left, pivot-1)  # pivot 보다 왼쪽 그룹을 다시 quickSort 한다
   quickSort(pivot+1, right) # pivot 보다 오른쪽 그룹을 다시 quickSort 한다
-  vis.pop()
+  # vis.pop()
 
 def partition(left, right):
 
@@ -32,31 +30,31 @@ def partition(left, right):
   while True:             # p < q 인 동안 하게 된다. 하지만 중간에 break 하므로 while True 를 쓰자
     while True:           # 왼쪽에서 pivot 보다 큰 값을 찾을때까지
       p += 1
-      vis.set_p(p)
+      # vis.set_p(p)
       if q < p: break
-      if p <= right: vis.compare(pi, p)
+      # if p <= right: vis.compare(pi, p)
       if p > right or array[p] > pivot: break 
       # 왼쪽에서 pivot 보다 큰 값을 찾았다
 
-      if p <= right: vis.set_left(p)
+      # if p <= right: vis.set_left(p)
 
     while True:           # 오른쪽에서 pivot 보다 작은 값을 찾을때까지
       q -= 1
-      vis.set_q(q)
+      # vis.set_q(q)
       if q < p: break
-      if q >= left: vis.compare(pi, q)
+      # if q >= left: vis.compare(pi, q)
       if q < left or array[q] < pivot: break
       # 오른쪽에서 pivot 보다 작은 값을 찾았다
 
-      if q >= left: vis.set_right(q)
+      # if q >= left: vis.set_right(q)
 
     if p >= q: break      # p 와 q 가 만날때까지 계속 진행한다
                           # 즉, p >= q 라면, 교환할 값이 없다는 뜻이다
 
-    vis.set_left(p)
-    vis.set_right(q)
+    # vis.set_left(p)
+    # vis.set_right(q)
 
-    vis.swap(p, q)
+    # vis.swap(p, q)
     array[p], array[q] = array[q], array[p] 
     # 이제 p 이하에는 pivot 보다 작은 값만, q 이상에는 pivot 보다 큰 값만 있다
 
@@ -66,14 +64,64 @@ def partition(left, right):
   # pivot 값은 왼쪽 그룹 중에 가장 큰 값이므로 q 위치로 옮긴다
   # left 가 q 와 같다면 pivot 보다 작은것이 하나도 없다는 뜻이므로 옮길 필요가 없다
   if left != q:
-    vis.swap(left, q, True)
+    # vis.swap(left, q, True)
     array[left], array[q] = array[q], array[left]
 
   return q  # 결정된 pivot 의 위치를 리턴한다
 
+''' 성능 측정
+count=100     elapsed= 0.000 creation= 0.00
+count=1000    elapsed= 0.003 creation= 0.00
+count=2000    elapsed= 0.005 creation= 0.00
+count=3000    elapsed= 0.007 creation= 0.00
+count=4000    elapsed= 0.010 creation= 0.00
+count=5000    elapsed= 0.013 creation= 0.00
+count=6000    elapsed= 0.015 creation= 0.00
+count=7000    elapsed= 0.021 creation= 0.01
+count=8000    elapsed= 0.021 creation= 0.01
+count=9000    elapsed= 0.023 creation= 0.01
+count=10000   elapsed= 0.030 creation= 0.01
+count=15000   elapsed= 0.043 creation= 0.01
+count=20000   elapsed= 0.059 creation= 0.01
+count=30000   elapsed= 0.096 creation= 0.02
+count=40000   elapsed= 0.125 creation= 0.03
+count=50000   elapsed= 0.156 creation= 0.03
+count=100000  elapsed= 0.419 creation= 0.07
+count=200000  elapsed= 0.690 creation= 0.14
+count=300000  elapsed= 1.115 creation= 0.20
+count=400000  elapsed= 1.670 creation= 0.26
+count=500000  elapsed= 2.235 creation= 0.32
+count=1000000 elapsed= 4.496 creation= 0.63
+count=2000000 elapsed= 9.740 creation= 1.25
+count=3000000 elapsed=15.635 creation= 2.06
+count=4000000 elapsed=20.053 creation= 2.60
+count=5000000 elapsed=26.747 creation= 3.55
+'''
 if __name__ == '__main__':
   seed('Hello')
-  vis = Visualizer('Quick Sort')
+
+  counts = [ 
+  # 10,20,30,50,100
+    100, 1000, 2000, 3000, 4000, 5000, 
+    6000, 7000, 8000, 9000, 10000, 15000, 
+    20000, 30000, 40000, 50000,
+    100000, 200000, 300000, 400000, 500000,
+    1000000, 2000000, 3000000, 4000000, 5000000,
+  ]
+  for count in counts:
+    startedOn = time()
+    array = [ randint(1, count) for _ in range(count) ]
+    creation = time() - startedOn
+    # shuffle(array)
+    # print('before:', array)
+    startedOn = time()
+    main()
+    elapsed = time() - startedOn
+    # print('after: ', array)
+    print(f'{count=:<7d} {elapsed=:6.3f} {creation=:5.2f}')    
+  exit() 
+
+  # vis = Visualizer('Quick Sort')
   while True:
     count = randint(20, 40)
     array = [ randint(1, 99) for _ in range(count) ]
