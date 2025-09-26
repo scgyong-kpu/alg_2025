b3dcfa03479cc8870ea177f5918e3ed93930f05c
KKY@Home <scgyong@tukorea.ac.kr>
2025-09-27 01:14:41 +0900

    ch32qs-6. pivot 의 위치가 확정되었으므로 회색으로 표시한다

--- week05_ch3/ch3_2_quick_sort.py
+++ week05_ch3/ch3_2_quick_sort.py
@@ -10,8 +10,8 @@ def main():
   count = len(array)
 
   vis.push(0, count-1)
-  partition(0, count-1)
-
+  pivot = partition(0, count-1)
+  vis.set_pivot(pivot)
   print('after :', array)
 
 def partition(left, right):
@@ -60,6 +60,8 @@ def partition(left, right):
   if left != q:
     vis.swap(left, q, True)
     array[left], array[q] = array[q], array[left]
+
+  return q  # 결정된 pivot 의 위치를 리턴한다
 
 if __name__ == '__main__':
   seed('Hello')
