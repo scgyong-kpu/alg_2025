9ea8136e2e193a6bb149e4f6cfbad30c8faa79be
KKY@Home <scgyong@tukorea.ac.kr>
2025-10-01 23:23:07 +0900

    ch33se-1. quick_sort 에 있는 partition 을 그대로 이용

--- week05_ch3/ch3_3_selection.py
+++ week05_ch3/ch3_3_selection.py
@@ -3,10 +3,16 @@ from pyvisalgo import SelectionVisualizer as Visualizer
 from time import time
 from random import randint, seed, shuffle
 
+import ch3_2_quick_sort as qs
+
 def main():
+  qs.array = array
+  qs.vis = vis
   print('before:', array)
   count = len(array)
   vis.push(0, count-1, n_th)
+  pi = qs.partition(0, count-1)
+  vis.set_pivot(pi)
 
 if __name__ == '__main__':
   seed('SelectionSeed')
