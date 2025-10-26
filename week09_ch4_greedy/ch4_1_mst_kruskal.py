6ae65f66ecaec1000fc1887ac2e203c4c7370dab
KKY@Home <scgyong@tukorea.ac.kr>
2025-10-27 08:34:07 +0900

    ---------------- Ch4 - 1. MST - Kruskal 시작. 간선들을 가중치를 기준으로 정렬시킨다 ----------------

--- week09_ch4_greedy/ch4_1_mst_kruskal.py
+++ week09_ch4_greedy/ch4_1_mst_kruskal.py
@@ -2,12 +2,21 @@ from pyvisalgo import KruskalVisualizer as Visualizer
 # from pyvisalgo import PlanarVisualizer as Visualizer
 import data_sample_cities as dsc
 
+def main():
+  # sorted_edges = sorted(edges, key=lambda e: e[2])
+  # print(sorted_edges)
+  vis.draw()
+  vis.wait(1000)
+  edges.sort(key=lambda e: e[2])
+  vis.sort_edges()
+  mst = []
+
 if __name__ == '__main__':
   vis = Visualizer('MST - Kruskal')
   while True:
     cities, edges = dsc.cities, dsc.edges
     vis.setup(vis.get_main_module())
-    vis.draw()
+    main()
     again = vis.end()
     if not again: break
     if vis.restart_lshift:
