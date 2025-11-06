367cd5285825e82ef036ba800d0fda37aa4dca28
KKY@Home <scgyong@tukorea.ac.kr>
2025-11-06 16:53:29 +0900

    Revert "ch51fw-7. k=0 갱신이 끝나면, k=1 을 들러서 가는 것이 더 빠른지 확인하여 갱신한다."
    This reverts commit 07d187df9ddaa0eef50179844291102a3c1853bb.

--- week11_ch5_dp/floyd_warshall.py
+++ week11_ch5_dp/floyd_warshall.py
@@ -49,7 +49,8 @@ class Floyd:
       self.dgraph[u][u] = 0
       vis.floyd_update(u, u, msec=200)
 
-    for k in range(N):
+    k = 0
+    if True:
       for i in range(N):
         for j in range(N):
           vis.floyd_compare(i, j, k)
