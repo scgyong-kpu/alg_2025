07d187df9ddaa0eef50179844291102a3c1853bb
KKY@Home <scgyong@tukorea.ac.kr>
2025-11-06 16:48:26 +0900

    ch51fw-7. k=0 갱신이 끝나면, k=1 을 들러서 가는 것이 더 빠른지 확인하여 갱신한다.

--- week11_ch5_dp/floyd_warshall.py
+++ week11_ch5_dp/floyd_warshall.py
@@ -49,8 +49,7 @@ class Floyd:
       self.dgraph[u][u] = 0
       vis.floyd_update(u, u, msec=200)
 
-    k = 0
-    if True:
+    for k in range(N):
       for i in range(N):
         for j in range(N):
           vis.floyd_compare(i, j, k)
